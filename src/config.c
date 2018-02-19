5df1a0e08075fa24b1ae82963ad75b6f452f8521
cszatma <christopherszatmary@gmail.com>
2018-02-19 16:21:21 -0500

    Added functions for loading the config file and reading values from it, created attic_config struct that will store all config values in memory

--- src/config.c
+++ src/config.c
@@ -2,22 +2,23 @@
 // Created by Christopher Szatmary on 2018-02-18.
 //
 
-#include <libconfig.h>
-#include <sys/stat.h>
 #include <string.h>
-#include "utils.h"
+#include <stdlib.h>
 #include "config.h"
 
 #define DEFAULT_LOCATION "/usr/local/attic"
 #define ATTIC_CONFIG_NAME "attic_config.cfg"
+#define ATTIC_VERSION_NUMBER "0.1.0"
+
+#define ATTIC_INSTALL_LOCATION "install_location"
+
+const attic_config *config_data = NULL;
+attic_config *config = NULL;
 
 int generate_new_config() {
     verbose_print("Generating config file.\n");
 
-    struct stat config_stats = {};
-
-    // Check if a config file already exists
-    if (stat(ATTIC_CONFIG_NAME, &config_stats) != -1) {
+    if (check_file_exists(ATTIC_CONFIG_NAME)) {
         printf("A config file already exists.\nAre you sure you want to recreate it? [y/n] ");
         char choice[3];
         scanf("%s", choice);
@@ -25,7 +26,7 @@ int generate_new_config() {
         // If user does not respond yes abort the process
         if (strcasecmp(choice, "y") != 0 && strcasecmp(choice, "yes") != 0) {
             verbose_print("Aborting create config.\n");
-            return -1;
+            return ACTION_ABORTED;
         }
     }
 
@@ -37,22 +38,86 @@ int generate_new_config() {
     root = config_root_setting(&cfg);
 
     // Add install location path to config
-    verbose_print("Adding intall location to config.\n");
-    path_config = config_setting_add(root, "install_location", CONFIG_TYPE_STRING);
+    verbose_print("Adding install location to config.\n");
+    path_config = config_setting_add(root, ATTIC_INSTALL_LOCATION, CONFIG_TYPE_STRING);
     config_setting_set_string(path_config, DEFAULT_LOCATION);
 
     // Write everything to the config file
+    verbose_print("Writing to file ...\n");
     if (!config_write_file(&cfg, ATTIC_CONFIG_NAME)) {
 
         // Handle any errors
         fprintf(stderr, "Error while writing file.\n");
+        print_config_error(fverbose_print, stderr, &cfg);
         config_destroy(&cfg);
-        return -1;
+        return ERROR_WRITE;
     }
 
     printf("New configuration successfully written to: %s\n", ATTIC_CONFIG_NAME);
 
     config_destroy(&cfg);
 
-    return 0;
+    return STATUS_OK;
+}
+
+int load_config() {
+    config_t cfg;
+
+    config_init(&cfg);
+
+    verbose_print("Reading from config file ...\n");
+    // Try reading from the config file
+    if (!config_read_file(&cfg, ATTIC_CONFIG_NAME)) {
+
+        // Handle errors
+        fprintf(stderr, "Error reading the config file.\n");
+        print_config_error(fverbose_print, stderr, &cfg);
+        config_destroy(&cfg);
+        return ERROR_READ;
+    }
+
+    verbose_print("Creating config struct\n");
+    config = malloc(sizeof(attic_config));
+
+    if (read_config_values(&cfg, config) != STATUS_OK) {
+        verbose_print("Unable to successfully read the config, aborting\n");
+        verbose_print("Freeing config struct\n");
+        free(config);
+        config_destroy(&cfg);
+        return ERROR_READ;
+    }
+
+    // Assign config to global constant pointer so that it can be read from anywhere but not modified
+    config_data = config;
+
+    return STATUS_OK;
+}
+
+int read_config_values(config_t *cfg, attic_config *config_struct) {
+    verbose_print("Reading config values\n");
+
+    // Try reading each value from the config file
+    // If any of them fail at any point exit the function
+    verbose_print("Reading install_location\n");
+    if (!config_lookup_string(cfg, ATTIC_INSTALL_LOCATION, &config_struct->install_location)) {
+        fprintf(stderr, "Error reading the install_location field from the config.\n");
+        return ERROR_READ;
+    }
+
+    verbose_print("Finished reading config values successfully\n");
+    return STATUS_OK;
+}
+
+void free_config() {
+    verbose_print("Freeing config struct\n");
+    free(config);
+}
+
+int print_config_error(print_func print, FILE *stream, config_t *cfg) {
+    return print(stream, "%s:%d - %s\n", config_error_file(cfg), config_error_line(cfg),
+          config_error_text(cfg));
+}
+
+char* get_version_number() {
+    return ATTIC_VERSION_NUMBER;
 }
--- src/config.h
+++ src/config.h
@@ -5,6 +5,21 @@
 #ifndef ATTIC_CONFIG_H
 #define ATTIC_CONFIG_H
 
+#include <stdio.h>
+#include <libconfig.h>
+#include "utils.h"
+
+typedef struct {
+    const char *install_location;
+} attic_config;
+
+extern const attic_config *config_data;
+
 int generate_new_config();
+int load_config();
+int read_config_values(config_t *cfg, attic_config *config_struct);
+void free_config();
+int print_config_error(print_func print, FILE *stream, config_t *cfg);
+char* get_version_number();
 
 #endif //ATTIC_CONFIG_H
