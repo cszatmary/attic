0fb9dc227d05822167d60299a4cec3e3944cb951
cszatma <christopherszatmary@gmail.com>
2018-02-21 15:07:54 -0500

    Setup option checking using getopt, program now parses arguments and chooses the appropriate function to execute

--- main.c
+++ main.c
@@ -1,49 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <pwd.h>
+#include <getopt.h>
 #include <string.h>
 #include "utils.h"
 #include "config.h"
 #include "install.h"
 
+typedef int (*command_func)(const char*);
+
+const char *attic_version = "attic -- version 0.1.0";
+
+void print_usage() {
+    fprintf(stderr, "Usage: attic [OPTION] [COMMAND] [FILE]\nTry 'attic --help' for more information.\n");
+}
+
 int main(int argc, char **argv) {
 
-//    char *src = argv[1];
-//    char *dest = argv[2];
-//
-//    int status = copy_file(src, dest, MAX_RX_PERM);
-//
-//    if (status == -1) {
-//        printf("Error copying. Process unsuccessful.\n");
-//        exit(1);
-//    } else {
-//        printf("Successfully copied file.\n");
-//    }
-//    printf("Status: %d", status);
-//    verbose_print("Install directory does not exist, creating it ...\n");
-//    mkdir(config_data->install_location, MAX_RX_PERM);
-//    perror("mkdir");
-//    verbose_print("Successfully created install directory at %s\n", config_data->install_location);
+    int opt;
+    int show_version; // Option flags
 
-    printf("User: %s\n", getpwuid(getuid())->pw_name);
-    printf("Home dir: %s\n", get_home_dir());
-    size_t size = strlen(get_home_dir()) + strlen(".attic") + 2;
-    printf("Size: %lu\n", size);
-    char path[size];
-    int status = join_path(get_home_dir(), ".attic", path, size);
-    printf("Joined path: %s\n", path);
-    printf("Status: %d\n", status);
+    struct option long_options[] = {
+            {"verbose", no_argument, NULL, 'v'},
+            {"version", no_argument, &show_version, 1},
+            {0, 0, 0, 0}
+    };
 
-    set_verbose(true);
-//    generate_new_config();
+    // Check options
+    while ((opt = getopt_long(argc, argv, "v", long_options, NULL)) != -1) {
+        switch (opt) {
+            case 0:
+                if (show_version) {
+                    printf("%s\n", attic_version);
+                    return EXIT_SUCCESS;
+                }
+                break;
+            case 'v':
+                set_verbose(true);
+                break;
+            case '?':
+            default:
+                fprintf(stderr, "Unknown option %c\n", opt);
+                return EXIT_FAILURE;
+        }
+    }
+
+    if (optind >= argc) {
+        print_usage();
+        return EXIT_FAILURE;
+    }
+
+    char *command_name = argv[optind++];
+    char *file_name = argv[optind];
+
+    if (file_name == NULL) {
+        print_usage();
+        return EXIT_FAILURE;
+    }
+
+    command_func command;
+
+    if (strcmp(command_name, "install") == 0) {
+        command = install;
+    } else {
+        fprintf(stderr, "ERROR: Unrecognized command '%s'\nTry 'attic --help' for more information.\n", command_name);
+        return EXIT_FAILURE;
+    }
+
     load_config();
-    printf("Install location is: %s\n", config_data->install_location);
-
-    install(argv[1]);
+    command(file_name);
 
     // Cleanup
     free_config();
 
-    return 0;
+    return EXIT_SUCCESS;
 }
