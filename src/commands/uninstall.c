0143f3770fd6f0b42f1f5837abb5cad8f9ca418b
cszatma <christopherszatmary@gmail.com>
2018-02-21 00:41:14 -0500

    Added uninstall.{c, h} which will contain functions for uninstall and unlink commands

---
+++ src/commands/uninstall.c
@@ -0,0 +1,5 @@
+//
+// Created by Christopher Szatmary on 2018-02-20.
+//
+
+#include "uninstall.h"
---
+++ src/commands/uninstall.h
@@ -0,0 +1,8 @@
+//
+// Created by Christopher Szatmary on 2018-02-20.
+//
+
+#ifndef ATTIC_UNINSTALL_H
+#define ATTIC_UNINSTALL_H
+
+#endif //ATTIC_UNINSTALL_H
