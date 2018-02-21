306ad6db868f2d0fc3bc6ec8689fc8aa46fe1815
cszatma <christopherszatmary@gmail.com>
2018-02-21 15:08:42 -0500

    Added setup.{c,h} which will contain functions for setting up attic

---
+++ src/commands/setup.c
@@ -0,0 +1,24 @@
+//
+// Created by Christopher Szatmary on 2018-02-21.
+//
+
+#include "setup.h"
+
+//    char *src = argv[1];
+//    char *dest = argv[2];
+//
+//    int status = copy_file(src, dest, MAX_RX_PERM);
+//
+//    if (status == -1) {
+//        printf("Error copying. Process unsuccessful.\n");
+//        exit(1);
+//    } else {
+//        printf("Successfully copied file.\n");
+//    }
+//    printf("Status: %d", status);
+//    verbose_print("Install directory does not exist, creating it ...\n");
+//    mkdir(config_data->install_location, MAX_RX_PERM);
+//    perror("mkdir");
+//    verbose_print("Successfully created install directory at %s\n", config_data->install_location);
+
+//    generate_new_config();
---
+++ src/commands/setup.h
@@ -0,0 +1,8 @@
+//
+// Created by Christopher Szatmary on 2018-02-21.
+//
+
+#ifndef ATTIC_SETUP_H
+#define ATTIC_SETUP_H
+
+#endif //ATTIC_SETUP_H
