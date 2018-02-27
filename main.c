2609545b31ef8dbdced4e95de66dd07e8bc41a81
Christopher Szatmary <christopherszatmary@gmail.com>
2018-02-27 18:14:18 -0500

    Added -h option which is the same as --help

--- main.c
+++ main.c
@@ -21,30 +21,30 @@ void print_usage() {
 int main(int argc, char **argv) {
 
     int opt;
-    int show_version, show_help; // Option flags
+    int show_version; // Option flags
 
     struct option long_options[] = {
             {"verbose", no_argument, NULL, 'v'},
             {"version", no_argument, &show_version, 1},
-            {"help", no_argument, &show_help, 1},
+            {"help", no_argument, NULL, 'h'},
             {0, 0, 0, 0}
     };
 
     // Check options
-    while ((opt = getopt_long(argc, argv, "v", long_options, NULL)) != -1) {
+    while ((opt = getopt_long(argc, argv, "vh", long_options, NULL)) != -1) {
         switch (opt) {
             case 0:
                 if (show_version) {
                     printf("%s\n", attic_version);
-                    return EXIT_SUCCESS;
-                } else if (show_help) {
-                    help();
                     return EXIT_SUCCESS;
                 }
                 break;
             case 'v':
                 set_verbose(true);
                 break;
+            case 'h':
+                help();
+                return EXIT_SUCCESS;
             case '?':
             default:
                 return EXIT_FAILURE;
