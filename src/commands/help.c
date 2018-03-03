fb9594e6dd02f32174a7fff8b926ba05953c28dc
cszatma <christopherszatmary@gmail.com>
2018-03-03 17:34:02 -0500

    Added attic list to help message, added descriptions of each command

--- src/commands/help.c
+++ src/commands/help.c
@@ -6,14 +6,16 @@
 #include "help.h"
 
 static const char *attic_help = "Example usage:\n"
-        "\tattic install [FILE]\n"
-        "\tattic uninstall [NAME]\n"
-        "\tattic link [NAME]\n"
-        "\tattic unlink [NAME]\n"
+        "\tattic install [FILE]\t-- Installs a program globally.\n"
+        "\tattic uninstall [NAME]\t-- Removes a program installed with attic.\n"
+        "\tattic link [NAME]\t-- Symlink a program installed with attic to '/usr/local/bin'.\n"
+        "\tattic unlink [NAME]\t-- Remove symlink from '/usr/local/bin'.\n"
+        "\tattic list\t\t-- Lists all programs installed with attic.\n"
         "\n"
         "Options:\n"
-        "\t-v, --verbose\tVerbose Mode\n"
-        "\t--version\tDisplay attic version\n";
+        "\t-v, --verbose\tVerbose Mode.\n"
+        "\t--version\tDisplay attic version.\n"
+        "\t-h, --help\tDisplays this help message.\n";
 
 void help() {
     printf("%s", attic_help);
